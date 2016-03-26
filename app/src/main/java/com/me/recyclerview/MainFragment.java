6a17010c7f0e6c650123ec5299f544a90c93c1f9
Braulio Mendez <Braulio Mendez>
2016-03-26 15:49:14 -0600

    Added onCreateView method

--- app/src/main/java/com/me/recyclerview/MainFragment.java
+++ app/src/main/java/com/me/recyclerview/MainFragment.java
@@ -1,9 +1,32 @@
 package com.me.recyclerview;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Braulio on 26/03/2016.
  */
 public class MainFragment extends Fragment {
+
+    @Bind(R.id.main_recycler) RecyclerView mainRecycler;
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
 }
